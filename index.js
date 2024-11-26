117ce366f6063b71181a445d9b05bc2bb868c27c
dorffdaniel <danielcontabill@hotmail.com>
2024-11-26 21:21:04 +0000

    alteraçao no ultimo btn adicionando js nele e adicionei imagem na pasta imagem

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const btnTroca = document.querySelector('.btnTroca');
 const formulario=document.querySelector('.formulario');
 const CorBranca = document.querySelectorAll('h1, h2, label');
 const main = document.querySelector('.main');
+const btnEnvioDoc = document.querySelector('.btn');
+const efetitoTrocaBtn= document.querySelector('.efeitoTroca');
 
 
 let textoOriginal = btnTroca.textContent;
@@ -23,4 +25,16 @@ btnTroca.addEventListener('click', ()=>{
         btnTroca.textContent = textoOriginal;
         btnTroca.classList.remove('backgroundBotao');
     }
+})
+
+btnEnvioDoc.addEventListener('click', ()=>{
+    efetitoTrocaBtn.innerHTML = '';
+    efetitoTrocaBtn.classList.add('circulo');
+
+    setTimeout(() => {
+        efetitoTrocaBtn.classList.remove('circulo');
+        //btnEnvioDoc.style.background='none'
+        btnEnvioDoc.classList.add('enviado');
+        efetitoTrocaBtn.classList.add('enviado');
+    }, 3000);
 })
