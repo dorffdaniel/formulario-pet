ee7769192dc62ba8f3828450997d1446fb60cd52
Dorffdaniel <danielcontabill@hotmail.com>
2025-05-24 00:03:06 +0100

    detalhes cadastro adicionado

---
+++ Pet.js
@@ -0,0 +1,86 @@
+class Pet {
+    constructor(tipo, cadast, genero, cor, nome, nomeTutor, email, cep, num) {
+        this._tipo = tipo;
+        this._cadast = cadast;
+        this._genero = genero;
+        this._cor = cor;
+        this._nome = nome;
+        this._nomeTutor = nomeTutor;
+        this._email = email;
+        this._cep = cep;
+        this._num = num;
+    }
+
+    get tipo() {
+        return this._tipo;
+    }
+
+    set tipo(tipo) {
+        this._tipo = tipo;
+    }
+
+    get cadast() {
+        return this._cadast;
+    }
+
+    set cadast(cadast) {
+        this._cadast = cadast;
+    }
+
+    get genero() {
+        return this._genero;
+    }
+
+    set genero(genero) {
+        this._genero = genero;
+    }
+
+    get cor() {
+        return this._cor;
+    }
+
+    set cor(cor) {
+        this._cor = cor;
+    }
+
+    get nome() {
+        return this._nome;
+    }
+
+    set nome(nome) {
+        this._nome = nome;
+    }
+
+    get nomeTutor() {
+        return this._nomeTutor;
+    }
+
+    set nomeTutor(nomeTutor) {
+        this._nomeTutor = nomeTutor;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    set email(email) {
+        this._email = email;
+    }
+
+    get cep() {
+        return this._cep;
+    }
+
+    set cep(cep) {
+        this._cep = cep;
+    }
+
+    get num() {
+        return this._num;
+    }
+
+    set num(num) {
+        this._num = num;
+    }
+
+}
--- index.js
+++ index.js
@@ -1,27 +1,29 @@
+let cadastroPet = [];
+
 const btnTroca = document.querySelector('.btnTroca');
-const formulario=document.querySelector('.formulario');
+const formulario = document.querySelector('.formulario');
 const CorBranca = document.querySelectorAll('h1, h2, label');
 const main = document.querySelector('.main');
-const btnEnvioDoc = document.querySelector('.btn');
-const efetitoTrocaBtn= document.querySelector('.efeitoTroca');
+const btnEnvioDoc = $('#btnEnviar');
+const efetitoTrocaBtn = document.querySelector('.efeitoTroca');
 
 
 let textoOriginal = btnTroca.textContent;
 
-btnTroca.addEventListener('click', ()=>{
+btnTroca.addEventListener('click', () => {
     formulario.classList.toggle('dark')
     main.classList.toggle('darkStrong');
-    
 
-    CorBranca.forEach((percorrer)=>{
+
+    CorBranca.forEach((percorrer) => {
         percorrer.classList.toggle('corBranca')
     })
 
-    if(btnTroca.textContent === textoOriginal){
-        btnTroca.textContent ='Versao Light';
+    if (btnTroca.textContent === textoOriginal) {
+        btnTroca.textContent = 'Versao Light';
         btnTroca.classList.add('backgroundBotao');
-        
-    }else{
+
+    } else {
         btnTroca.textContent = textoOriginal;
         btnTroca.classList.remove('backgroundBotao');
     }
@@ -31,19 +33,76 @@ btnTroca.addEventListener('click', ()=>{
 let cadastro = [];
 
 
-btnEnvioDoc.addEventListener('click', ()=>{
+btnEnvioDoc.on('click', () => {
+
+    let temporarioEspec = [];
+    let temporCadastro = [];
+    let temporGenero = [];
+
+    for (let i = 1; i <= 5; i++) {
+        if ($('#especie' + i).is(':checked')) {
+            temporarioEspec.push($('#especie' + i).val())
+        }
+    }
+
+    for (let i = 0; i <= 2; i++) {
+        if ($('#cad' + i).is(':checked')) {
+            temporCadastro.push($('#cad' + i).val())
+        }
+    }
+
+    for (let i = 0; i <= 2; i++) {
+        if ($('#genero' + i).is(':checked')) {
+            temporGenero.push($('#genero' + i).val())
+        }
+    }
 
 
-    efetitoTrocaBtn.innerHTML = '';
-    efetitoTrocaBtn.classList.add('circulo');
+    cadastroPet.push(new Pet(
+        temporarioEspec,
+        temporCadastro,
+        temporGenero,
+        $('#cor').val(),
+        $('#nomePet').val(),
+        $('#nomeTutor').val(),
+        $('#email').val(),
+        $('#cep').val(),
+        $('#num').val()
+    ))
 
-    setTimeout(() => {
-        efetitoTrocaBtn.classList.remove('circulo');
-        //btnEnvioDoc.style.background='none'
-        btnEnvioDoc.classList.add('enviado');
-        efetitoTrocaBtn.classList.add('enviado');
-    }, 3000);
+
+
+
+    mostrarModal()
+
+
 })
+
+
+function mostrarModal() {
+    $('#modalPet').modal('show');
+    
+    let msg = '';
+
+    cadastroPet.forEach(element => {
+        msg += `<ul>`
+        msg += `<li> tipo: ${element.tipo}</li>`
+        msg += `<li> cadastrado: ${element.cadast}</li>`
+        msg += `<li> genero: ${element.genero}</li>`
+        msg += `<li> cor: ${element.cor}</li>`
+        msg += `<li> pet: ${element.nome}</li>`
+        msg += `<li> tutor: ${element.nomeTutor}</li>`
+        msg += `<li> email: ${element.email}</li>`
+        msg += `<li> cep: ${element.cep}</li>`
+        msg += `<li> numero: ${element.num}</li>`
+        msg += `</ul>`
+
+    })
+
+
+    $('#modalRes').html(msg);
+}
+
 
 
 
